60c3736f60ab3c0184fb73fdcf48942e86885b78
Vinicius Figueira <figueiravinicius@hotmail.com>
2024-12-09 12:16:45 -0300

    criação do game

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const thais = document.querySelector('.thais')
+
+
+function jump() {
+  mario.classList.add('jump')
+
+  setTimeout(() => {
+    mario.classList.remove('jump')
+  }, 500) 
+}
+
+const loop = setInterval(() => {
+
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+  const pipePosition = pipe.offsetLeft;
+  
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
+
+    mario.style.animation = 'none'
+    mario.style.bottom = `${marioPosition}px`
+
+    mario.src = './images/game-over.png'
+    mario.style.width = '80px'
+    mario.style.marginLeft = '50px'
+
+    thais.style.display = 'block'
+
+    clearInterval(loop)
+  }
+
+}, 10);
+
+document.addEventListener('click', jump)
+document.addEventListener('keydown', jump)
+document.addEventListener('touch', jump)
